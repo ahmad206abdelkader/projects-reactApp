f1852e9ecf5c4d1bc399d7388e1d9be4716ec59a
ahmad206abdelkader <ahmad11iphone11abdelkader11@gmail.com>
2023-02-21 14:29:05 -0500

    add Cart for the project

--- food-order/src/components/Layout/HeadrCartButton.js
+++ food-order/src/components/Layout/HeadrCartButton.js
@@ -1,16 +1,24 @@
+import { useContext } from "react";
 import CartIcon from "../Card/CartIcon";
+import CartContext from "../../store/cart-context";
 import Classes from './HeaderCartButton.module.css'
 
-const HeaderCartButton = props => {
-   return( <button className={Classes.button}>
-    <span className={Classes.icon}>
-        <CartIcon />
-        
-    </span>
-    <span>Your Cart</span>
-    <span className={Classes.badge}>3</span> 
-   </button>
-   );
+const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    return (<button className={Classes.button} onClick={props.onClick} >
+        <span className={Classes.icon}>
+            <CartIcon />
+
+        </span>
+        <span>Your Cart</span>
+        <span className={Classes.badge}>{numberOfCartItems}</span>
+    </button>
+    );
 };
 
 export default HeaderCartButton;    
--- food-order/src/components/UI/modal.js
+++ food-order/src/components/UI/modal.js
@@ -1,9 +1,9 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import classes from './modal.module.css';
 import ReactDOM from 'react-dom';
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />
+    return <div className={classes.backdrop} onClick={props.onClose} />
 };
 
 const ModalOverlay = (props) => {
@@ -16,7 +16,7 @@ const PortalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
     return <Fragment>
-        {ReactDOM.createPortal(<Backdrop />, PortalElement)}
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, PortalElement)}
         {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, PortalElement)}
     </Fragment>
 };
