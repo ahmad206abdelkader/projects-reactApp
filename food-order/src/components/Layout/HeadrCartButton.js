4ea43c2e71ef2235ec470032752c9c5cb9fae81d
ahmad206abdelkader <ahmad11iphone11abdelkader11@gmail.com>
2023-02-23 09:41:02 -0500

    finish update commit

--- food-order/src/components/Layout/HeadrCartButton.js
+++ food-order/src/components/Layout/HeadrCartButton.js
@@ -1,16 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Card/CartIcon";
 import CartContext from "../../store/cart-context";
 import Classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+    const [BtnIsHighLighted, setBtnIsHigtLited] = useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
-    return (<button className={Classes.button} onClick={props.onClick} >
+
+
+    const btnClasses = `${Classes.button} ${BtnIsHighLighted ? Classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+
+        setBtnIsHigtLited(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHigtLited(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
+    return (<button className={btnClasses} onClick={props.onClick} >
         <span className={Classes.icon}>
             <CartIcon />
 
--- food-order/src/components/Meals/MealItem/MealItemForm.js
+++ food-order/src/components/Meals/MealItem/MealItemForm.js
@@ -1,20 +1,40 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/input';
 import classes from './MealItemForm.module.css';
 
 
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: 'amount',
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInoutRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInoutRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().lenght === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            return '';
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
+
+    return (<form className={classes.form} onSubmit={submitHandler}>
+        <Input
+            ref={amountInoutRef}
+            label="Amount" input={{
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />
 
         <button>+ add</button>
-    </form>
+        {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
+    </form>);
 };
 
 export default MealItemForm;
