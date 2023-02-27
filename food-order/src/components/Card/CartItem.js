af28e4d40c8568463be810c9159d77934ba0eb00
ahmad206abdelkader <ahmad11iphone11abdelkader11@gmail.com>
2023-02-27 09:45:31 -0500

    update icon commit

--- food-order/src/components/Card/CartItem.js
+++ food-order/src/components/Card/CartItem.js
@@ -1,9 +1,29 @@
 import classes from './CartItem.module.css';
 
+
 const CartItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
 
+    const icon = props.amount;
+
+    const TruchIcon = (props) => {
+
+        if (icon <= 1) {
+            return (<span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg></span>)
+        } else {
+            return (<span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z" />
+            </svg></span>)
+        }
+
+    };
+
+
+
     return (
+
         <li className={classes['cart-item']}>
             <div>
                 <h2>{props.name}</h2>
@@ -13,7 +33,7 @@ const CartItem = (props) => {
                 </div>
             </div>
             <div className={classes.actions}>
-                <button onClick={props.onRemove}>−</button>
+                <button onClick={props.onRemove}><TruchIcon /></button>
                 <button onClick={props.onAdd}>+</button>
             </div>
         </li>
--- food-order/src/components/UI/modal.js
+++ food-order/src/components/UI/modal.js
@@ -1,11 +1,15 @@
 import { Fragment, useState } from 'react';
 import classes from './modal.module.css';
+//reactDOM for definsition of constent
 import ReactDOM from 'react-dom';
 
+
+//the backdrop it background transbarent
 const Backdrop = (props) => {
     return <div className={classes.backdrop} onClick={props.onClose} />
 };
 
+//the modaloverlay it window white for cart
 const ModalOverlay = (props) => {
     return <div className={classes.modal}>
         <div className={classes.content}>{props.children}</div>
