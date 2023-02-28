8d0e94fa9d3758134b004f39fe7147a0a77b9575
ahmad206abdelkader <ahmad11iphone11abdelkader11@gmail.com>
2023-02-28 14:42:48 -0500

    add @media

--- food-order/src/App.js
+++ food-order/src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Card/Cart";
 import CartProvider from "./store/CartProvider";
+import classes from "./components/UI/about.module.css";
+
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -20,13 +22,22 @@ function App() {
 
   return (
     <CartProvider>
+
       {cartIsShown && < Cart onClose={hideCartHandler} />}
       <Header onShowCart={ShowCartHandler} />
       <main>
         <Meals />
+
+
       </main>
+      <ul className={classes.Ul}>
+        <li>created by</li>
+        <li>ahmad abdelkader</li>
+        <li>00962780754976</li>
+      </ul>
 
     </CartProvider>
+
   );
 }
 
