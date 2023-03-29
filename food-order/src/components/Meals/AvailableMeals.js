5242d5a5416a628dc2fc86745db891fd2d645a0d
ahmad206abdelkader <ahmad11iphone11abdelkader11@gmail.com>
2023-03-29 13:13:42 -0400

    update backend commit

--- food-order/src/components/Meals/AvailableMeals.js
+++ food-order/src/components/Meals/AvailableMeals.js
@@ -8,10 +8,17 @@ import MealItem from "./MealItem/MealItem";
 const AvaliableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://food-order12-default-rtdb.firebaseio.com/meals.json');
+
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      };
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,12 +36,23 @@ const AvaliableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+
   }, []);
 
   if (isLoading) {
     return <section className={classes.MealsLoading}>
       <p>Loading ...</p>
+    </section>
+  };
+
+  if (httpError) {
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
     </section>
   }
 
